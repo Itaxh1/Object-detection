30d82b275c5611f962b58c29055ed960e117acf6
ASHWIN KUMAR <meetashwin2000@gmail.com>
2019-04-04 22:06:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(name="imageai",
+      version='2.0.1',
+      description='A flexible Computer Vision and Deep Learning library for applications and systems.',
+      url="https://moses.specpal.science",
+      author='Moses Olafenwa and John Olafenwa',
+      license='MIT',
+      packages= find_packages(),
+      zip_safe=False
+
+      )
